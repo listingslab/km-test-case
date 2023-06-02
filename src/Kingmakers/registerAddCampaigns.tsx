3595ce59cd24401d19b508f134cddceb2f1c407e
milky <listingslab@gmail.com>
2023-06-02 13:31:37 +0200

    Update registerAddCampaigns.tsx

--- src/Kingmakers/registerAddCampaigns.tsx
+++ src/Kingmakers/registerAddCampaigns.tsx
@@ -10,14 +10,17 @@ export const registerAddCampaigns = () => {
         try {
             let severity = "success"
             let message = "Campaigns added. Nice."
+            
             if (!data){
                 severity = "warning"
                 message = "Please pass an array of Campaigns in your method call to AddCampaigns()"
             }
+
             store.dispatch(setPwaKey({ key: "notifyer", value: {
                 severity,
                 message 
             }}))  
+            
         } catch (error: any) {
             store.dispatch(setPwaKey({ key: "notifyer", value: {
                 severity:"error",
